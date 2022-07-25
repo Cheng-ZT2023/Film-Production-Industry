c0607d697f88f3bd89dbb660fc3a6bfbe1ee5fe6
User <User@DESKTOP-EKDL3KF>
2022-07-25 15:07:21 +0800

    Add project files.

---
+++ Film Production Industry/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Film_Production_Industry.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
